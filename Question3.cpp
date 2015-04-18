ae6f09cd7ac3f6f431207abd67fe115023160a23
RO169 <rowen.rungiah@gmail.com>
2015-04-18 15:14:04 +0200

    overloaded add functions created

--- Question3.cpp
+++ Question3.cpp
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //                         COMPUTER METHODS 3 PRACTICAL TEST 1 (MOCK)
-// Name:
-// Student No:
-// Date: 
+// Name: RO169
+// Student No: 213 000 000
+// Date: 04/2015
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
 
@@ -17,11 +17,34 @@ using namespace std;
 //     either order) and return a fraction.  
 // 3.3 create a driver program to test your overloaded > operators and your overloaded 
 
-class Fraction {
+class Fraction 
+{
 private:
 	int num;				// numerator;
 	int denom;				// denominator;
 public:
-	Fraction(int n, int d) : num(n), denum(d) { };
+	Fraction();
+	Fraction(int n, int d) : num(n), denom(d) { };
 	void print() { cout << num << "/" << denom; };
+	                                        
+	bool operator>(Fraction& F);                       //operator overloading
+	Fraction add(int, Fraction);                       //Function overloading-Accepting data types
+	Fraction add(Fraction, int);                       //in any order
 };
+
+bool Fraction::operator>(Fraction& F)
+{
+
+}
+
+Fraction Fraction::add(int num1, Fraction F2)
+{
+	F2.num += num1*F2.denom;
+	return F2;
+}
+
+Fraction Fraction::add(Fraction F3, int num2)
+{
+	F3.num += num2*F3.denom;
+	return F3;
+}
